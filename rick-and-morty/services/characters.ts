ed82e37a72ffa52caae5a6e9ce6983931dc4a765
Charles Ramos <44650185+CharlesrRamos@users.noreply.github.com>
2023-04-02 22:48:09 -0300

    Fix, get Character

--- rick-and-morty/services/characters.ts
+++ rick-and-morty/services/characters.ts
@@ -1,16 +1,26 @@
 import client from "./client";
 import { Characters } from "../models/character";
 
-export const getCharacters = async (limit?: number) => {
+export const getCharacters = async (limit?: number, paramPage?: number) => {
   var page = 1;
-  if (limit) page = limit / 10;
+  if (paramPage) {
+    page = paramPage;
+  } else {
+    if (limit) page = limit / 10;
+  }
+
   const params = {
     page,
   };
+
   const charactersData = await client.get<Characters>("character", { params });
   var results = charactersData.data.results;
-  // if (limit) {
-  //   results = results.filter((x) => x.id <= limit);
-  // }
+  return results;
+};
+
+export const getInfoCharacters = async () => {
+  const charactersData = await client.get<Characters>("character");
+  var results = charactersData.data.info;
+
   return results;
 };
